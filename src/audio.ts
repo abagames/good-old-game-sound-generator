edf122b603b162989ee6881a6408624f3ba9a582
ABA Games <cs8k-cyu@asahi-net.or.jp>
2022-02-06 15:55:28 +0900

    Add audioContext param

--- src/audio.ts
+++ src/audio.ts
@@ -4,10 +4,11 @@ export let playInterval: number;
 export let quantize: number;
 let isStarted = false;
 
-export function init() {
-  const AudioContext =
-    window.AudioContext || (window as any).webkitAudioContext;
-  audioContext = new AudioContext();
+export function init(_audioContext: AudioContext = undefined) {
+  audioContext =
+    _audioContext == null
+      ? new (window.AudioContext || (window as any).webkitAudioContext)()
+      : _audioContext;
   setTempo(150);
   quantize = 0.5;
 }
